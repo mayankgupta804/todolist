2ee674aa5db4f3eb0f7eedaa0c706f43a44e1b3d
mayankgupta804 <mayank.gupta804@gmail.com>
2018-05-05 15:07:41 +0530

    Add models + relevant migrations and create urls to the todo app

--- todolist/urls.py
+++ todolist/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', include('todos.urls')),
+    # url(r'^$', include('todos.urls')),
     url(r'^todos/', include('todos.urls')),
     url(r'^admin/', admin.site.urls),
 ]
