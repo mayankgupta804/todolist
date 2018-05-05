293d507ea44ee8cbb0f5b00768ac032517503a17
mayankgupta804 <mayank.gupta804@gmail.com>
2018-05-05 17:41:52 +0530

    Successfully created a shitty todo-app with CRUD functionality

--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,31 @@
 from django.conf.urls import url
 from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from .models import Todo
 
 def index(request):
-    return HttpResponse('Hello World')
+    todos = Todo.objects.all()[:10]
+    context = {
+        'todos' : todos
+    }
+    return render(request, 'index.html', context)
+
+def details(request, id):
+    todo = Todo.objects.get(id=id)
+    context = {
+        'todo' : todo
+    }
+    return render(request, 'details.html', context)
+
+def add(request):
+    if(request.method == 'POST'):
+        title = request.POST['title']
+        text = request.POST['text']
+
+        todo = Todo(title=title, text=text)
+        todo.save()
+
+        return redirect('/todos')
+    else:
+        return render(request, 'add.html')
+
